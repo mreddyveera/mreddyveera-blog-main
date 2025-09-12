ee0d8a58212bfeda54bf4d1749a9188185949996
mreddyveera <reddyveeramanikanta54304@gmail.com>
2025-09-12 22:33:01 +0530

    Make the dashboard private

---
+++ mreddyveera-blog-main/client/src/components/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import {useSelector} from "react-redux";
+import {Outlet,Navigate} from "react-router-dom";
+
+function PrivateRoute(){
+    const {currentUser}=useSelector((state)=>state.user);
+    console.log(currentUser);
+
+    return (
+        <>
+        {currentUser?<Outlet/>:<Navigate to="/signin"/>
+}
+        </>
+    )
+}
+export default PrivateRoute;
